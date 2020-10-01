38ff0d536df9e41388b0c48720776ac5fc653b38
João Henrique <joaohenrique.p.almeida@gmail.com>
2020-10-01 07:37:16 -0300

    Change config for deploy in heroku

--- config/database.js
+++ config/database.js
@@ -93,7 +93,7 @@ module.exports = {
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
       database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1)),
-      ssl: Env.get('DB_SSL', true)
+      ssl: Env.get('DB_SSL', 'true')
     }
   }
 
